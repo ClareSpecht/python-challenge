59e726649c94bf30ba5da2802dbcf68eb5d8b56d
Clare Specht <clarespecht21@gmail.com>
2022-01-08 13:54:40 -0500

    CSV mod pulled in. Coded out instructions for PyBank

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,6 +1,36 @@
+#Setup Workspace to Interact with CSV File Types
 import os
 
 import csv
 
+#Locate Specific budget_data file
 csvpath = os.path.join('Resources','budget_data.csv')
 
+with open(csvpath) as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=',')
+
+    print('Financial Analysis')
+    print('----------------------------')
+    
+    #Ignore Column Headers
+    csvheader = next(csvreader)
+    
+    #Calculate total number of months included in dataset (Determine number of rows in dataset)
+    rows = len(list(csvreader))
+    print(f"Total Months: {rows}")
+
+    #Calculate net total amount of "Profit/Losses"
+    print(f"Total: $")
+
+    #Calculate changes in "Profit/Losses"
+
+    #Calculate average change in "Profit/Losses"
+    print(f"Average  Change: $")
+
+    #Find greatest increase in profits
+    #Print Date and Amount
+    print(f"Greatest Increase in Profits: ")
+
+    #Find greatest decrease in profits
+    #Print Date and Amount
+    print(f"Greatest Decrease in Profits: ")
