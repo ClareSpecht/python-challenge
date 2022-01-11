db8dd87cf023c0d0fedbb21cbb0d8c35aa44bdca
Clare Specht <clarespecht21@gmail.com>
2022-01-11 00:14:58 -0500

    Text files created. PyPoll results formatted and completed.

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,4 +1,4 @@
-#Setup Workspace to Interact with CSV File Types
+#Setup Workspace to Interact with CSV Mod
 import os
 
 import csv
@@ -8,29 +8,55 @@ csvpath = os.path.join('Resources','budget_data.csv')
 
 with open(csvpath) as csvfile:
     csvreader = csv.reader(csvfile, delimiter=',')
-
-    print('Financial Analysis')
-    print('----------------------------')
     
     #Ignore Column Headers
     csvheader = next(csvreader)
+
+    # Initialize variables to store outputs
+    Rows = 0
+    Total = 0
+    PreviousProfit = 0
+    ChangeTotal = 0
+    increase = 0
+    decrease = 0
+
+    for row in csvreader:
+
+        #Calculate total number of months included in dataset (Determine number of rows in dataset)
+        Rows = Rows + 1
+        
+        #Calculate net total amount of "Profit/Losses"
+        MonthlyProfit = int(row[1])
+        Total = Total + MonthlyProfit
+
+        #Calculate changes in "Profit/Losses"
+        Change = MonthlyProfit - PreviousProfit
+        ChangeTotal = ChangeTotal + Change #????Currently just returning profit of final date????
+
+        #Find greatest increase/decrease in profits
+        if increase <= Change:
+            increase = Change
+            BestMonth = row[0]
+        elif decrease >= Change:
+            decrease = Change
+            WorstMonth = row[0]
+
+        #Store profit as PreviousProfit
+        PreviousProfit = MonthlyProfit
     
-    #Calculate total number of months included in dataset (Determine number of rows in dataset)
-    rows = len(list(csvreader))
-    print(f"Total Months: {rows}")
-
-    #Calculate net total amount of "Profit/Losses"
-    print(f"Total: $")
-
-    #Calculate changes in "Profit/Losses"
-
     #Calculate average change in "Profit/Losses"
-    print(f"Average  Change: $")
+    AvgChange = round((ChangeTotal / Rows), 2)
 
-    #Find greatest increase in profits
-    #Print Date and Amount
-    print(f"Greatest Increase in Profits: ")
+#Print Analysis to terminal
+Analysis = ['Financial Analysis', '----------------------------', f"Total Months: {Rows}", 
+    f"Total: ${Total}", f"Average  Change: ${AvgChange}", 
+    f"Greatest Increase in Profits: {BestMonth} (${increase})", f"Greatest Decrease in Profits: {WorstMonth} (${decrease})"]
 
-    #Find greatest decrease in profits
-    #Print Date and Amount
-    print(f"Greatest Decrease in Profits: ")
+print(*Analysis, sep=os.linesep)
+
+#Export Analysis to text file
+output_path = os.path.join("analysis", "Financial Analysis.txt")
+    
+with open(output_path, 'w') as txtfile:
+    for result in range(len(Analysis)):
+        txtfile.write(Analysis[result] + '\n')
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,6 +1,60 @@
+#Setup Workspace to Interact with CSV Mod
 import os
 
 import csv
 
+#Locate Specific election_data file
 csvpath = os.path.join('Resources','election_data.csv')
 
+with open(csvpath) as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=',')
+    
+    #Ignore Column Headers
+    csvheader = next(csvreader)
+
+    # Initialize variables to store outputs
+    Votes = 0
+    Candidates = []
+    CandVotes = []
+
+    for row in csvreader:
+        
+        #Calculate total number of votes (Determine number of rows in dataset)
+        Votes = Votes + 1
+
+        #Compile List of candidates who received votes
+        mycandidate = row[2]
+        if mycandidate not in Candidates:
+            Candidates.append(mycandidate)
+            CandVotes.append(1) #Adds first vote to candidates vote count
+        
+        #Count up total number of votes each candidate won
+        else:
+            CandVotes[Candidates.index(mycandidate)] = CandVotes[Candidates.index(mycandidate)] + 1
+
+    #Calculates percentages of votes each candidate won
+    percentages = [(round(myvotes/Votes*100, 3)) for myvotes in CandVotes]
+
+#Create results text
+str1 = "Election Results"
+str2 = "-------------------------"
+str3 = (f"Total Votes: {Votes}")
+
+#Index each list to print out all information relevant informatin per candidate
+str4 = []
+for breakdown in range(len(Candidates)):
+    str4.append(f'{Candidates[breakdown]}: {percentages[breakdown]}% ({CandVotes[breakdown]})')
+    
+str5 = (f"Winner: ")
+
+Analysis = [str1, str2, str3, str2, *str4, str2, str5, str2]
+
+#Print Analysis to terminal
+print(*Analysis, sep=os.linesep)
+
+#Export Analysis to text file
+output_path = os.path.join("analysis", "Election Results.txt")
+    
+with open(output_path, 'w') as txtfile:
+    for result in range(len(Analysis)):
+        txtfile.write(Analysis[result] + '\n')
